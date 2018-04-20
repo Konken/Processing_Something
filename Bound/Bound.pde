5c01afa18136ca74ae7074d584c4fec4833f26aa
Sekiyu_ROD <kentaro032414@gmail.com>
2018-04-20 17:25:20 +0900

    つらい

--- Bound/Bound.pde
+++ Bound/Bound.pde
@@ -1,24 +1,98 @@
 final float gravity=9.8;
-Ball b=new Ball(random(width),0,10,10);
+Ball[]b=new Ball[500];
+Obj o=new Obj(400,400,200);
 void setup() {
 	fullScreen();
+	for(int i=0;i<b.length;i++){
+		b[i]=new Ball(random(width),random(500),10,random(5,10),random(5,10));
+	}
 }
 
 void draw() {
-	b.update();
+	background(0);
+	o.drawEllipse();
+	for(int i=0;i<b.length;i++){
+		b[i].update();
+		if(o.collision(b[i].x,b[i].y)){
+			b[i].angle=o.getAngle(b[i].x,b[i].y);
+			b[i].x=o.x+cos(radians(o.give(b[i].x,b[i].y)))*o.rad/2;
+			b[i].y=o.y+sin(radians(o.give(b[i].x,b[i].y)))*o.rad/2;
+		}
+	}
 }
 
 class Ball{
-	float x,y,size,spy;
-	Ball(float x,float y,float size,float spy){
+	float x,y,size,spx,spy;
+	float nowy;
+	float angle=90;
+	float xb=1;
+	float yb=1;
+	Ball(float x,float y,float size,float spy,float spx){
 		this.x=x;
 		this.y=y;
 		this.size=size;
 		this.spy=spy;
+		spx=0;
+		nowy=y;
 	}
 	void update(){
+		fill(255);
+		stroke(255);
 		ellipse(x, y,10,10);
-		spy+=gravity/60;
+		spx=cos(radians(angle))*5*xb;
+		spy=sin(radians(angle))*5*yb;
+		x+=spx;
 		y+=spy;
+		if(60<frameCount){
+			if(width<x){
+				xb*=-0.6;
+				x=width;
+			}
+			else if(x<0){
+				xb*=-0.6;
+				x=0;
+			}
+			if(height<y){
+				yb*=-.6;
+				y=height;
+			}
+			else if(y<0){
+				yb*=-0.6;
+				y=0;
+			}
+		}
+	}
+}
+class Obj{
+	float x,y,rad;
+	Obj(float x,float y,float rad){
+		this.x=x;
+		this.y=y;
+		this.rad=rad;
+	}
+	void drawEllipse(){
+		noFill();
+		stroke(255);
+		ellipse(x,y,rad,rad);
+	}
+	boolean collision(float ballX,float ballY){
+		return dist(x,y,ballX,ballY)<rad/2;
+	}
+	float getAngle(float ballX,float ballY){
+		float temp=give(ballX,ballY);
+		if(temp<90){
+			//temp+=90;
+		}
+		else if(temp==90){
+			temp=0;
+		}
+		else{
+			//temp-=90;
+		}
+		return temp;
+	}
+	float give(float ballX,float ballY){
+		float temp=-degrees(atan2(abs(y-ballY),abs(x-ballX)));
+		return temp;
 	}
 }
--- Bound/build-tmp/source/Bound.java
+++ Bound/build-tmp/source/Bound.java
@@ -15,27 +15,101 @@ import java.io.IOException;
 public class Bound extends PApplet {
 
 final float gravity=9.8f;
-Ball b=new Ball(random(width),0,10,10);
+Ball[]b=new Ball[500];
+Obj o=new Obj(400,400,200);
 public void setup() {
 	
+	for(int i=0;i<b.length;i++){
+		b[i]=new Ball(random(width),random(500),10,random(5,10),random(5,10));
+	}
 }
 
 public void draw() {
-	b.update();
+	background(0);
+	o.drawEllipse();
+	for(int i=0;i<b.length;i++){
+		b[i].update();
+		if(o.collision(b[i].x,b[i].y)){
+			b[i].angle=o.getAngle(b[i].x,b[i].y);
+			b[i].x=o.x+cos(radians(o.give(b[i].x,b[i].y)))*o.rad/2;
+			b[i].y=o.y+sin(radians(o.give(b[i].x,b[i].y)))*o.rad/2;
+		}
+	}
 }
 
 class Ball{
-	float x,y,size,spy;
-	Ball(float x,float y,float size,float spy){
+	float x,y,size,spx,spy;
+	float nowy;
+	float angle=90;
+	float xb=1;
+	float yb=1;
+	Ball(float x,float y,float size,float spy,float spx){
 		this.x=x;
 		this.y=y;
 		this.size=size;
 		this.spy=spy;
+		spx=0;
+		nowy=y;
 	}
 	public void update(){
+		fill(255);
+		stroke(255);
 		ellipse(x, y,10,10);
-		spy+=gravity/60;
+		spx=cos(radians(angle))*5*xb;
+		spy=sin(radians(angle))*5*yb;
+		x+=spx;
 		y+=spy;
+		if(60<frameCount){
+			if(width<x){
+				xb*=-0.6f;
+				x=width;
+			}
+			else if(x<0){
+				xb*=-0.6f;
+				x=0;
+			}
+			if(height<y){
+				yb*=-.6f;
+				y=height;
+			}
+			else if(y<0){
+				yb*=-0.6f;
+				y=0;
+			}
+		}
+	}
+}
+class Obj{
+	float x,y,rad;
+	Obj(float x,float y,float rad){
+		this.x=x;
+		this.y=y;
+		this.rad=rad;
+	}
+	public void drawEllipse(){
+		noFill();
+		stroke(255);
+		ellipse(x,y,rad,rad);
+	}
+	public boolean collision(float ballX,float ballY){
+		return dist(x,y,ballX,ballY)<rad/2;
+	}
+	public float getAngle(float ballX,float ballY){
+		float temp=give(ballX,ballY);
+		if(temp<90){
+			//temp+=90;
+		}
+		else if(temp==90){
+			temp=0;
+		}
+		else{
+			//temp-=90;
+		}
+		return temp;
+	}
+	public float give(float ballX,float ballY){
+		float temp=-degrees(atan2(abs(y-ballY),abs(x-ballX)));
+		return temp;
 	}
 }
   public void settings() { 	fullScreen(); }
